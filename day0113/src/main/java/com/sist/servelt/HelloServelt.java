45b8aedb49bfe5a0e541b9b1cd85cdd3ec0294b0
khs17 <khs17@DESKTOP-CGMNMTI>
2022-01-14 09:20:58 +0900

    day0113[이미지 슬라이드, db ajax]

---
+++ day0113/src/main/java/com/sist/servelt/HelloServelt.java
@@ -0,0 +1,50 @@
+package com.sist.servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServelt
+ */
+@WebServlet("/Hello.do")
+public class HelloServelt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServelt() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	 
+		
+		response.setContentType("text/plaine"); 
+		PrintWriter out =response.getWriter();
+		out.print("Hello");
+		out.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ day0113/src/main/webapp/getBook.jsp
@@ -0,0 +1,4 @@
+<%@ page language="java" contentType="text/plaine; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+ 축구의역사,굿스포츠,7000
+ 
